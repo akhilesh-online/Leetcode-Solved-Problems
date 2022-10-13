b7753943883ff2505ee0a277f5a86b2d367b2bfe
Akhilesh Kumar <akhilesh1kr@gmail.com>
2022-10-13 15:03:07 +0530

    Time: 58 ms (56.52%), Space: 13.9 MB (11.26%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.py
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -1,12 +1,12 @@
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
         count = 0
-        repeat = {} # dict of number in nums as key and number of their repeatition as value
+        repeat = {}  # dict of 'values in nums' as key and 'their repeatition' as value
         for n in nums:
             if n in repeat:
                 count += repeat[n]
-                repeat[n] += 1
+                repeat[n] += 1  # repeatition value of key 'n' increased by 1 
             else:
-                repeat[n] = 1
+                repeat[n] = 1  # inserted a new key value pair as {n: 1} in dict
         return count
             
