db224a70c7479d7f16c47c826d41cb734c8871ef
Akhilesh Kumar <akhilesh1kr@gmail.com>
2022-10-18 12:24:46 +0530

    Time: 142 ms (92.30%), Space: 14.4 MB (62.92%) - LeetHub

--- 1603-design-parking-system/1603-design-parking-system.py
+++ 1603-design-parking-system/1603-design-parking-system.py
@@ -1,15 +1,16 @@
 class ParkingSystem:
+    __slots__=['_parking']
 
     def __init__(self, big: int, medium: int, small: int):
-        self.parking = {
+        self._parking = {
             1 : big,
             2 : medium,
             3 : small
         }
 
     def addCar(self, carType: int) -> bool:
-        if self.parking[carType] > 0:
-            self.parking[carType] -= 1
+        if self._parking[carType] > 0:
+            self._parking[carType] -= 1
             return True
         return False
 
