df0f0abbb45f8d27d3a8bbe31ff2b08e01add471
Akhilesh Kumar <akhilesh1kr@gmail.com>
2022-11-14 15:39:00 +0530

    Time: 472 ms (42.05%), Space: 16.4 MB (9.52%) - LeetHub

--- 1476-subrectangle-queries/1476-subrectangle-queries.py
+++ 1476-subrectangle-queries/1476-subrectangle-queries.py
@@ -1,14 +1,16 @@
 class SubrectangleQueries:
 
     def __init__(self, rectangle: List[List[int]]):
-        self.rectangle = rectangle
+        self.rectangle = copy.deepcopy(rectangle)
+        self.history = []
 
     def updateSubrectangle(self, row1: int, col1: int, row2: int, col2: int, newValue: int) -> None:
-        for i in range(row1,row2+1):
-            for j in range(col1,col2+1):
-                self.rectangle[i][j] = newValue
+        self.history.append([row1, col1, row2, col2, newValue])
 
     def getValue(self, row: int, col: int) -> int:
+        for his in reversed(self.history):
+            if his[0] <= row <= his[2] and his[1] <= col <= his[3]:
+                return his[4]
         return self.rectangle[row][col]
 
 
