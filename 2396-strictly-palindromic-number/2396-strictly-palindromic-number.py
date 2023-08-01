1220f7ffb6adc73b9439eed33e359ac6405d8ef0
Akhilesh Kumar <akhilesh1kr@gmail.com>
2023-08-01 22:43:18 +0530

    Time: 38 ms (90.37%), Space: 16.2 MB (91.56%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.py
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.py
@@ -1,23 +1,10 @@
 class Solution:
     def isStrictlyPalindromic(self, n: int) -> bool:
-        for i in range(2, n-1):
-            res = self.isPalindrome(self.getPalindrome(n, i))
-            if res == False:
-                return False
-        return True
-    
-    def getPalindrome(self, n: int, base: int) -> str:
-        result = ''
-        while n > 0:
-            result = str(n%base) + result
-            n = n//base
-        return result
-
-    def isPalindrome(self, n: str) -> bool:
-        l = len(n)
-        count = 0
-        while count <= l/2:
-            if n[count] != n[l-1-count]:
-                return False
-            count += 1
+        for base in range(2, n-1):
+            res = []
+            k = n
+            while k > 0:
+                res.append(k % n)
+                k //= base
+            if res != res[::-1]: return False
         return True
