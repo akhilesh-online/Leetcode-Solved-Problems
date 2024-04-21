d1082e204a328db97941a7f3b24174528ec1bb03
Akhilesh Kumar <akhilesh1kr@gmail.com>
2024-04-21 10:21:01 +0530

    Time: 36 ms (50.13%), Space: 16.5 MB (93.61%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -1,8 +1,3 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        result = ""
-        l = min(len(word1), len(word2))
-        for i in range(l):
-            result += word1[i] + word2[i]
-        result += word1[l:] + word2[l:]
-        return result
+        return ''.join(a+b for a, b in zip_longest(word1, word2, fillvalue=''))
