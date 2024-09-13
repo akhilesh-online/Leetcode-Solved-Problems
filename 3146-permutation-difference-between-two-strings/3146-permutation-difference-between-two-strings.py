e3a3fe5f42fce926ff7434a70b0df0cb9eb3d1d2
Akhilesh Kumar <akhilesh1kr@gmail.com>
2024-09-13 12:35:56 +0530

    Time: 40 ms (49.10%), Space: 16.5 MB (28.56%) - LeetHub

--- 3146-permutation-difference-between-two-strings/3146-permutation-difference-between-two-strings.py
+++ 3146-permutation-difference-between-two-strings/3146-permutation-difference-between-two-strings.py
@@ -1,4 +1,4 @@
 class Solution:
     def findPermutationDifference(self, s: str, t: str) -> int:
         pos = {ch : i for i, ch in enumerate(t)}
-        return sum([abs(i - pos[s[i]]) for i in range(len(s))])
+        return sum([abs(i - pos[ch]) for i, ch in enumerate(s)])
